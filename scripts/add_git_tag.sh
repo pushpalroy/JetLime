ecccf95780587aa2c92bf1d534d39ecd8f0e10f9
Pushpal Roy <pushpalroy2007@gmail.com>
2024-01-21 03:48:54 -0600

    Add script to add git tag

---
+++ scripts/add_git_tag.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Script to add annotated tag with version number to the main branch
+# This script should be executed with the correct version number after every release to MavenCentral
+TAG="1.0.5"
+COMMENT="Release 1.0.5"
+BRANCH="main"
+
+# Fetch the latest changes in the repository
+git fetch
+
+# Check if the main branch exists and switch to it
+if git rev-parse --verify $BRANCH; then
+    git checkout $BRANCH
+    git pull origin $BRANCH
+else
+    echo "Branch '$BRANCH' does not exist."
+    exit 1
+fi
+
+# Add the annotated tag
+git tag -a $TAG -m "$COMMENT"
+
+# Push the tag to remote repository
+git push origin $TAG
