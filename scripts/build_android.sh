0633d42853e33e700ce37697f137e751dfa2eee1
Pushpal Roy <pushpalroy2007@gmail.com>
2024-09-07 02:49:00 -0600

    [ci] migrate to matrix strategy for build jobs

--- scripts/build_android.sh
+++ scripts/build_android.sh
@@ -1,15 +1,33 @@
 #!/bin/bash
 
+# Exit the script on any error
+set -e
+
 # Navigate to the root directory of the project
 cd "$(dirname "$0")/.." || exit
 
 # Create distributions folder in the root directory (if not already present)
-mkdir -p distributions
+mkdir -p distributions/android
 
 # Build Android App
 echo "Building Android App 📱"
-./gradlew :sample:composeApp:assembleDebug
+./gradlew :sample:composeApp:assembleDebug --console=plain --stacktrace
 
-# Verify and copy the APK to the distributions folder
-echo "Verifying Android App"
-cp sample/composeApp/build/outputs/apk/debug/composeApp-debug.apk distributions/jetlime-sample-android.apk
+# Check if the build was successful
+if [ $? -eq 0 ]; then
+    echo "Android build successful."
+
+    # Verify and copy the APK to the distributions folder
+    APK_PATH="sample/composeApp/build/outputs/apk/debug/composeApp-debug.apk"
+
+    if [ -f "$APK_PATH" ]; then
+        cp "$APK_PATH" distributions/android/jetlime-sample-android.apk
+        echo "Android APK copied to distributions/android/jetlime-sample-android.apk"
+    else
+        echo "APK not found at expected path: $APK_PATH"
+        exit 1
+    fi
+else
+    echo "Android build failed."
+    exit 1
+fi
--- scripts/build_web.sh
+++ scripts/build_web.sh
@@ -1,16 +1,34 @@
 #!/bin/bash
 
+# Exit the script on any error
+set -e
+
 # Navigate to the root directory of the project
 cd "$(dirname "$0")/.." || exit
 
 # Build Web App
 echo "Building Web App 🌎"
-./gradlew :sample:composeApp:wasmJsBrowserDistribution
+./gradlew :sample:composeApp:wasmJsBrowserDistribution --console=plain --stacktrace
 
-# Create the distributions/jetlime-web folder in the root directory
-mkdir -p distributions/jetlime-web
+# Check if the build was successful
+if [ $? -eq 0 ]; then
+    echo "Web build successful."
 
-# Copy the production executable to the distributions folder
-cp -r sample/composeApp/build/dist/wasmJs/productionExecutable/ distributions/jetlime-web/
+    # Create the distributions/jetlime-web folder in the root directory
+    mkdir -p distributions/web
 
-echo "Web app build and copied to distributions/jetlime-web"
+    # Path to the production executable
+    WEB_EXECUTABLE_PATH="sample/composeApp/build/dist/wasmJs/productionExecutable/"
+
+    # Verify and copy the production executable to the distributions folder
+    if [ -d "$WEB_EXECUTABLE_PATH" ]; then
+        cp -r "$WEB_EXECUTABLE_PATH" distributions/web/
+        echo "Web app copied to distributions/web"
+    else
+        echo "Web build output not found at expected path: $WEB_EXECUTABLE_PATH"
+        exit 1
+    fi
+else
+    echo "Web build failed."
+    exit 1
+fi
