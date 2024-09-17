9fa8cb7af4556984003c008e16c9fcab2ae0b7ac
Pushpal Roy <pushpalroy2007@gmail.com>
2024-09-17 06:57:03 -0600

    [lib] fix: update to JVM 17 for compile and android target

--- sample/composeApp/build.gradle.kts
+++ sample/composeApp/build.gradle.kts
@@ -34,7 +34,7 @@ kotlin {
   androidTarget {
     @OptIn(ExperimentalKotlinGradlePluginApi::class)
     compilerOptions {
-      jvmTarget.set(JvmTarget.JVM_1_8)
+      jvmTarget.set(JvmTarget.JVM_17)
     }
   }
 
@@ -148,8 +148,8 @@ android {
   }
 
   compileOptions {
-    sourceCompatibility = JavaVersion.VERSION_1_8
-    targetCompatibility = JavaVersion.VERSION_1_8
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
   }
   buildFeatures {
     compose = true
