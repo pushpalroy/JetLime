0d5bced8923c0c355be43410ed39ea21753be4be
Pushpal Roy <pushpalroy2007@gmail.com>
2025-08-11 02:57:48 +0530

    Release version 4.0.0 (#54)

--- sample/composeApp/build.gradle.kts
+++ sample/composeApp/build.gradle.kts
@@ -112,7 +112,7 @@ android {
   defaultConfig {
     applicationId = "com.pushpal.jetlime.sample"
     minSdk = 21
-    targetSdk = 34
+    targetSdk = 36
     versionCode = 1
     versionName = "1.0"
 
